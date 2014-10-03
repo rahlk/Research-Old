5f460c5f1c16950e9c40909b0aba148f3f28de96
Rahul <Krishna>
2014-10-03 18:54:21 -0400

    Added a weighting function; 5 nearest neighbors found with the weights

--- Weighted kNN/kNN_main.py
+++ Weighted kNN/kNN_main.py
@@ -3,37 +3,49 @@ German words! It's almost Oktober! :)
 """
 
 from __future__ import division
-from lib    import *
-from demos  import *
-from counts import *
-from fi     import *
-from table import *
+from ahadist import *
 from lib import isa
 import numpy as np
 import scipy as sp
 import sys
 from scipy.spatial.distance import pdist
+
 sys.dont_write_bytecode = True
 squareform=sp.spatial.distance.squareform
 
 class o: 
   def __init__(self,**d): self.__dict__.update(d)
+  
+class weights(object):  
+  def __init__(self,tbl):
+    self.tbl=tbl
+  def getWeights(self):
+    pass
 
 class uberschrift: ## German for header
   def __init__(self):
     pass
   
+  def unwrap(self,t0):
+    rows = map(lambda x :x.cells, t0._rows)
+    H=[];
+    for i in t0.headers:
+      H.append(i.__dict__['name'])
+    rows.insert(0,H)
+    return {rows[0][z]:[p[z] for p in rows[1:] if z<=len(rows[1])-1] for z in xrange(0,len(rows[0]))}
+        
+    
   def pairs(self,lst):
     for j in lst[0:]:
       last = j
       for i in lst[0:]:
         yield last, i
       
-  def dist(self,lst1, lst2):
-    def IB1(a,b):
-      if not isinstance(a,bool) and not isinstance(a, str) or not isinstance(b,bool) and not isinstance(b, str): return (a-b)**2
-      else: return not(a==b)
-    return -np.sum([IB1(elem1, elem2) for elem1, elem2 in zip(lst1, lst2)])
+  def dist(self,lst1, lst2, weights):
+    def IB1(a,b,w):
+      if not isinstance(a,bool) and not isinstance(a, str) or not isinstance(b,bool) and not isinstance(b, str): return w*(a-b)**2
+      else: return w*(not a==b)
+    return -np.sum([IB1(lst1[x], lst2[x], weight) for x, weight in enumerate(weights)])
     
 
 class haupt: ## Haupt translates roughly to main
@@ -43,7 +55,8 @@ class haupt: ## Haupt translates roughly to main
   uber=uberschrift(); # Create an alias for Uberschrift class.
   pairs = uber.pairs; # Generate all possible combinations of data, I took the original pairs function and modified it slightly.
   pdistVect=[] # pairwise distance matrix.
-  
+  ad=ahadist()
+  weights=ad.weights(tbl)
   #--------------------------------------------------
   # When in doubt, test the kNN with the following list, CAUTION: Use 1 maybe 2 neighbors at most.
   ##rows=[[1,2],[1,3],[2,1],[3,1],[9,8],[10,9],[8,10]]
@@ -54,7 +67,7 @@ class haupt: ## Haupt translates roughly to main
     ## one>two gets only the lower triangle of the entire matrix, since the 
     #  distance matrix is basically symmetric, this is all we need.
     if one>two: 
-      pdistVect.append(uber.dist(one, two))
+      pdistVect.append(uber.dist(one, two, weights))
   
   # Now get the square pairwise distance matrix. This is a MxM matrix, where M is the number of rows.
   # By the way, the pdistVect above is a M(M-1)/2 sized vector
