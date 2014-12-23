d0705bb721f25190d45a2e3d8d923d886837f5b0
rahlk <i.m.ralk@gmail.com>
2014-12-23 18:55:18 -0500

    Printfunction from the future

--- Defects/treatments.py
+++ Defects/treatments.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from _imports import *
 from contrastset import *
 from dectree import *
@@ -17,15 +18,23 @@ def _treatments(dir='./Data'):
   # Decision Tree ===============================================================
   t = discreteNums(train_DF, map(lambda x: x.cells, train_DF._rows))
   myTree = tdiv(t)
-#  showTdiv(myTree)
+  showTdiv(myTree)
 
   # Testing data ===============================================================
   testCase = test_df._rows
   
-  for tC in [testCase[0]]:
+  for tC in [testCase[1]]:
    loc = drop(tC, myTree)
 
-  print loc
+  # Reach the tree top =========================================================
+  newNode = loc;
+  branches = [];
+  while newNode.lvl >= 0:
+    print();
+    branches.append(newNode);
+    newNode = newNode.up;
+  print branches
+
 
 if __name__ == '__main__':
   _treatments(dir = './Data')
